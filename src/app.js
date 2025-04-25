25975572e8e9bdde272897a5cad029a95a79baa3
Nitish Kumar <nitish441999@gmail.com>
2025-04-25 11:49:29 +0530

    somechnage

--- src/app.js
+++ src/app.js
@@ -6,11 +6,20 @@ dotenv.config();
 
 const app = express();
 
+const allowedOrigins = ["http://localhost:8080"]; 
+
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
-    methods: ["GET", "POST", "PATCH", "DELETE"],
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "PATCH", "DELETE"],
   })
 );
 
@@ -21,15 +30,18 @@ app.use(cookieParser());
 
 // Routers
 import userRouter from "./routes/user.route.js";
-app.use("/api/v1/users", userRouter);
+app.use("/api/v1/users", userRouter); 
+
 
 app.get("/", (req, res) => {
   res.send("Server started via GET request");
 });
 
+
 app.use((req, res) => {
   res.status(404).json({ success: false, message: "Route not found" });
 });
+
 
 app.use((err, req, res, next) => {
   res.status(err.status || 500).json({ success: false, message: err.message });
