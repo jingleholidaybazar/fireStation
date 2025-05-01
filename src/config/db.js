0e0b88a54ecffa5552f305f95a5614286a35d1f4
jingleholiday <jingleholidaybazar@gmail.com>
2025-05-01 15:06:59 +0530

    somechnage

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
   try {
     const connectionIntance = await mongoose.connect(
-      `${process.env.MONGODB_URI}${DB_NAME}`
+      `${process.env.MONGODB_URL}${DB_NAME}`
     );
     console.log(
       `\n MongoDB Connected !! DB Host ${connectionIntance.connection.host}`
